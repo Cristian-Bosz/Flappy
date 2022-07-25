eaad27bdd564e7c34a3d2ef8bfbc866f7037dc7a
franjz <agustinzoric0@gmail.com>
2022-07-25 13:00:53 -0300

    Home maquetada

--- app/view/pages/home.php
+++ app/view/pages/home.php
@@ -1,43 +1,59 @@
 <?php
 include_once URL_APP . '/view/custom/header.php';
 include_once URL_APP . '/view/custom/navbar.php';
-
+//var_dump($datos);
 ?>
-<pre>
-    <?php var_dump($datos);?>
-</pre>
 <div class="container">
 <div class="row">
-    <div class="col-md-3">  
-    <div class="container">
-    <img class="rounded-circle w-25" src="<?php echo URL_PROJECT . '/' . $datos['perfil']->fotoPerfil?>"/>
-<br>
-<div class="text-left"> <?= $datos['perfil']->nombreCompleto?></div>
-<div class="">
-    <a href="#"> Publicaciones  0</a> <br>
-    <a href="#"> Me gustas  0</a>
-</div>
-    </div>
-    </div>
-
-
-
-    <div class="col-md-6">  
-        <div class="container">
-        <a href="<?php echo URL_PROJECT?>/perfil"><img class="rounded-circle w-25" src="<?php echo URL_PROJECT . '/' . $datos['perfil']->fotoPerfil?>"/></a>
-        <form action="">   
-            <textarea class="form-control" placeholder="¿Que estas pensando?" name="post"></textarea>
-                <div>
-                    <div class="input-file">
-                        <input type="file" name="imagen" id="imagen">
-                    </div>
-                    <span class="ms-1">Subir foto</span>
+    <div class="col-lg-3 fondo">
+        <div class="cardP overflow-hidden">
+        <div class="h-50px bg-profile"></div>
+        <div class="card-body pt-0">
+            <div class="text-center">
+                <div class="avatar avatar-lg mt-n5 mb-3">
+                    <a href="#">
+                        <img src="<?php echo URL_PROJECT . '/' . $datos['perfil']->fotoPerfil?>" class="avatar-img rounded border border-white border-3" alt="fotoPerfil">
+                    </a>
                 </div>
-            <button class="btn btn-primary float-end">Publicar</button>
-        </form>
+            <h5><?= $datos['perfil']->nombreCompleto?></h5>
+            <small class="text-muted"><?= $datos['usuario']->username ?></small>
+            <p class="text-muted mt-2">El saber no ocupa espacio</p>
+            <div class="hstack gap-2 gap-xl-3 justify-content-center">
+                <div>
+                    <h6 class="mb-0">2</h6>
+                    <small>Posts</small>
+                </div>
+                <div class="vr"></div>
+                <div>
+                    <h6 class="mb-0">259</h6>
+                    <small>Likes</small>
+                </div>
+            </div>
+            </div>
         </div>
     </div>
+    </div>
+    <div class="col-md-8 col-lg-6 vstack gap-4">
+        <div class="card card-body">
+            <div class="d-flex mb-3">
+                <div class="avatar me-2">
+                    <a href="#">
+                        <img src="<?php echo URL_PROJECT . '/' . $datos['perfil']->fotoPerfil?>" alt="fotoPerfilPublicacion" class="avatar-img rounded-circle">
+                    </a>
+                </div>
+                <form action="" class="w-100">
+                    <textarea class="form-control pe-4 border-0" rows="2" data-autoresize="" placeholder="¿Que estas pensando...?" style="height: 61px;"></textarea>
+                    <label for="subirImg" class="mt-3">
+                        <p class="btn bg-light py-1 px-2 mb-0"><i class="fa-solid fa-image text-success pe-2"></i> Foto</p>
+                        <input type="file" id="subirImg"  style="display: none" />
+                    </label>
+                    <button type="submit" class="btn btn-primary float-end mt-3">Publicar</button>
+                </form>
+            </div>
+        </div>
 
+    </div>
+    
     <div class="col-md-3">  </div>
 </div>
 </div>
