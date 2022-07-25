11c9520076c70585b4e81cd7f94768bd219df143
franjz <agustinzoric0@gmail.com>
2022-07-25 16:00:55 -0300

    Perfil maquetacion2

--- app/view/pages/perfil/perfil.php
+++ app/view/pages/perfil/perfil.php
@@ -1,6 +1,7 @@
 <?php
 include_once URL_APP . '/view/custom/header.php';
 include_once URL_APP . '/view/custom/navbar.php';
+
 ?>
 <div class="container mt-3">
     <div class="row">
@@ -63,13 +64,6 @@ include_once URL_APP . '/view/custom/navbar.php';
     </div>
 
 </div>
-
-<?php if($datos['usuario']->usuario_id == $_SESSION['logueado']) : ?>
-    <a href="<?php echo URL_PROJECT ?>/perfil/<?php $datos['usuario']->username ?>"> 
-        <img class="w-25 rounded-circle" src="<?php echo URL_PROJECT . '/' . $datos['perfil']->fotoPerfil ?>">
-    </a>
-<?php endif ?>
-
 
 <?php
 include_once URL_APP . '/view/custom/footer.php';
